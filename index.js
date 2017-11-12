ce0c84ee6116fbecdfaa1b6b395f138b8dd8123d
carlitos321 <ca.cabrera321@gmail.com>
2017-11-12 22:34:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,16 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
+
   if (string === 'I love you, Grandma.') {
     return "I love you, too."
+  } 
+
+  else if (string === string.toUpperCase()) {
+    return 'YES INDEED!'
+  }
+
+  else {
+    return 'I can\'t hear you!'
   }
 }
